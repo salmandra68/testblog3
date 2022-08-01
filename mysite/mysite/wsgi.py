afb876727e1c194134b3b923306b5e2cf9920c4d
salmandra <salmandra@gmail.com>
2022-08-01 22:17:05 +0200

    upgrade2

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -11,9 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
 
 from django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
+
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'MYBLOG.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MYBLOG.settings")
